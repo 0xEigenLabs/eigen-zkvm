979d66c0c1455c0bed32d4dc52b321604c2ed0ec
eigmax <stephen@ieigen.com>
2022-04-28 19:19:46 +0800

    feat(governace): fix run_recursive

--- test/run_recursive.sh
+++ test/run_recursive.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
+#set -e
 
 ## build
 
-cargo build
+cargo build --release
 
+POWER=20
 CUR_DIR=$(cd $(dirname $0);pwd)
 ZKIT="${CUR_DIR}/../target/debug/zkit"
 
 WORKSPACE=/tmp/recursive
 rm -rf $WORKSPACE && mkdir -p $WORKSPACE
 
-#SRS=${CUR_DIR}/../keys/setup_2^10.key
-SRS=${CUR_DIR}/../setup_2^24.key
+SRS=${CUR_DIR}/../keys/setup_2^${POWER}.key
+
+if [ ! -f $SRS  ]; then
+   curl https://universal-setup.ams3.digitaloceanspaces.com/setup_2^${POWER}.key -o $SRS
+fi
 
 echo "1. compile circuit"
 ${ZKIT} compile -i multiplier.circom --O2=full -o $WORKSPACE
 
-echo "2. generate witness"
+echo "2. export verification key"
+${ZKIT} export_verification_key -s ${SRS} -c $WORKSPACE/multiplier.r1cs -o $WORKSPACE/vk.bin
+
+echo "3. generate witness"
 for wtns in `ls $CUR_DIR/recursive/input`
 do
     input=$CUR_DIR/recursive/input/$wtns
     node ${WORKSPACE}/multiplier_js/generate_witness.js \
         ${WORKSPACE}/multiplier_js/multiplier.wasm \
         $input/input.json $input/witness.wtns
-    ${ZKIT} prove -c $WORKSPACE/multiplier.r1cs -w $input/witness.wtns -b $input/proof.bin -s ${SRS}
+    ${ZKIT} prove -c $WORKSPACE/multiplier.r1cs -w $input/witness.wtns -b $input/proof.bin -s ${SRS} -t rescue
 done
 
-echo "3. collect old proof list"
+echo "4. collect old proof list"
 OLD_PROOF_LIST=$WORKSPACE/old_proof_list.txt
 > $OLD_PROOF_LIST
 
@@ -38,11 +46,13 @@ do
     let "i++"
 done
 
-echo "4. export vk"
-${ZKIT} export_recursive_verification_key -c $i -i 2 -s ${SRS} -o $WORKSPACE/recursive_vk.bin
+cat $OLD_PROOF_LIST
 
-echo "5. generate recursive proof"
-${ZKIT} recursive_prove -s ${SRS} -f $OLD_PROOF_LIST  -v $WORKSPACE/vk.bin -n $WORKSPACE/recursive_proof.bin  -j $WORKSPACE/proof.json
+echo "5. export recursive vk"
+${ZKIT} export_recursive_verification_key -c $i -i 3 -s ${SRS} -o $WORKSPACE/recursive_vk.bin
 
-echo "6. verify"
+echo "6. generate recursive proof"
+${ZKIT} recursive_prove -s ${SRS} -f $OLD_PROOF_LIST  -v $WORKSPACE/vk.bin -n $WORKSPACE/recursive_proof.bin  -j $WORKSPACE/recursive_proof.json
+
+echo "7. verify"
 ${ZKIT} recursive_verify -p $WORKSPACE/recursive_proof.bin -v $WORKSPACE/recursive_vk.bin
