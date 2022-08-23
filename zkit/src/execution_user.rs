ca86c791c470141fba7ddbf3a32a27581126b06f
eigmax <stephen@ieigen.com>
2022-08-23 21:45:49 +0800

    docs: change org name

--- zkit/src/execution_user.rs
+++ zkit/src/execution_user.rs
@@ -36,6 +36,8 @@ pub fn execute_project(
         flag_verbose: config.flag_verbose,
         inspect_constraints: config.inspect_constraints_flag,
         prime: config.prime,
+        // https://github.com/iden3/circom/commit/8f140c1dec7975b339bfe17c1f08d8081b913560
+        flag_old_heuristics: false
     };
     let (exporter, vcp) = build_circuit(program_archive, build_config)?;
     if config.r1cs_flag {
