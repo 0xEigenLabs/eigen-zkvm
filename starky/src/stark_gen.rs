8d4af8d46a684614491e6a4ec03fd7810f5603ed
eigmax <stephen@ieigen.com>
2022-12-24 00:14:49 +0800

    chore: mute some log

--- starky/src/stark_gen.rs
+++ starky/src/stark_gen.rs
@@ -835,6 +835,7 @@ pub mod tests {
     }
 
     #[test]
+    #[ignore]
     fn test_stark_connection() {
         let mut pil = load_json::<PIL>("data/connection.pil.json").unwrap();
         let mut const_pol = PolsArray::new(&pil, PolKind::Constant);
