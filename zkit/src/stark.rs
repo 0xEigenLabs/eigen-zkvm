7206a1b6a143217439aa876d830b24e6dc4afc13
eigmax <stephen@ieigen.com>
2022-12-25 20:56:36 +0800

    fix some code style

--- zkit/src/stark.rs
+++ zkit/src/stark.rs
@@ -1,9 +1,9 @@
 use starky::{
+    merklehash_bn128::MerkleTreeBN128,
     polsarray::{PolKind, PolsArray},
     stark_gen::StarkProof,
     stark_setup::StarkSetup,
     stark_verify::stark_verify,
-    merklehash_bn128::MerkleTreeBN128,
     transcript_bn128::TranscriptBN128,
     types::*,
 };
@@ -22,7 +22,8 @@ pub fn prove(
     cm_pol.load(cm_pol_file.as_str()).unwrap();
 
     let stark_struct = load_json::<StarkStruct>(stark_struct.as_str()).unwrap();
-    let mut setup = StarkSetup::<MerkleTreeBN128>::new(&const_pol, &mut pil, &stark_struct).unwrap();
+    let mut setup =
+        StarkSetup::<MerkleTreeBN128>::new(&const_pol, &mut pil, &stark_struct).unwrap();
     let starkproof = StarkProof::<MerkleTreeBN128>::stark_gen::<TranscriptBN128>(
         &cm_pol,
         &const_pol,
