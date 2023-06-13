0501e40bee78afc3e22c195a9e2d2d98db8443b5
cyl19970726 <15258378443@163.com>
2023-06-13 11:42:09 +0000

    add comment

--- zkit/src/stark.rs
+++ zkit/src/stark.rs
@@ -23,17 +23,23 @@ pub fn prove(
     zkin: &String,
 ) -> Result<()> {
     let mut pil = load_json::<PIL>(pil_file.as_str()).unwrap();
+    // load the const polynomials which writen in pil and compiled by 'pil-com'
     let mut const_pol = PolsArray::new(&pil, PolKind::Constant);
     const_pol.load(const_pol_file.as_str()).unwrap();
 
+    // load the commit polynomials which writen in pil and compiled by 'pil-com'
     let mut cm_pol = PolsArray::new(&pil, PolKind::Commit);
     cm_pol.load(cm_pol_file.as_str()).unwrap();
 
     let stark_struct = load_json::<StarkStruct>(stark_struct.as_str()).unwrap();
     match stark_struct.verificationHashType.as_str() {
         "BN128" => {
+
+            // only the const polynomials will engage in the setup stage 
             let mut setup =
                 StarkSetup::<MerkleTreeBN128>::new(&const_pol, &mut pil, &stark_struct).unwrap();
+
+            // generate the stark proof
             let starkproof = StarkProof::<MerkleTreeBN128>::stark_gen::<TranscriptBN128>(
                 &cm_pol,
                 &const_pol,
@@ -57,6 +63,7 @@ pub fn prove(
             assert_eq!(result, true);
             println!("verify the proof done");
 
+            // translate the .pil to circom
             let opt = pil2circom::StarkOption {
                 enable_input: false,
                 verkey_input: false,
