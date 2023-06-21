c8b016d75506d48efd98ddea8ae0e6a1f77898e1
cyl19970726 <15258378443@163.com>
2023-06-21 07:47:14 +0000

    update fib_agg sh

--- test/test_aggregation_fibonacci_verifier.sh
+++ test/test_aggregation_fibonacci_verifier.sh
@@ -29,17 +29,17 @@ if [ ! -f $BIG_SRS ]; then
 fi
 
 echo "1. compile circuit"
-${ZKIT} compile -i $CIRCUIT.circom -l "../starkjs/node_modules/pil-stark/circuits.bn128" -l "../starkjs/node_modules/circomlib/circuits" --O2=full -o $WORKSPACE
+${ZKIT} compile -i ../starkjs/circuits/$CIRCUIT.circom -l "../starkjs/node_modules/pil-stark/circuits.bn128" -l "../starkjs/node_modules/circomlib/circuits" --O2=full -o $WORKSPACE
 
 echo "2. export verification key"
-${ZKIT} export_verification_key -s ${SRS} -c $WORKSPACE/${CIRCUIT}.r1cs -v $WORKSPACE/vk.bin
+${ZKIT} export_verification_key -s ${SRS} -c $WORKSPACE/${CIRCUIT}.r1cs --v $WORKSPACE/vk.bin
 
 echo "3. generate each proof"
 for wtns in `ls $CUR_DIR/aggregation/${CIRCUIT}`
 do
     input=$CUR_DIR/aggregation/${CIRCUIT}/$wtns
-    ${ZKIT} calculate_witness -w ${WORKSPACE}/${CIRCUIT}_js/$CIRCUIT.wasm -i ${input}/input.json -o $WORKSPACE/witness.wtns
-    ${ZKIT} prove -c $WORKSPACE/${CIRCUIT}.r1cs -w $input/witness.wtns -b $input/proof.bin -s ${SRS} -t rescue
+    ${ZKIT} calculate_witness -w ${WORKSPACE}/${CIRCUIT}_js/$CIRCUIT.wasm -i ${input}/input.json -o $input/witness.wtns
+    ${ZKIT} prove -c $WORKSPACE/${CIRCUIT}.r1cs -w $input/witness.wtns --b $input/proof.bin -s ${SRS} -t rescue
 done
 
 echo "4. collect old proof list"
@@ -57,13 +57,13 @@ done
 cat $OLD_PROOF_LIST
 
 echo "5. export aggregation vk"
-${ZKIT} export_aggregation_verification_key -c $i -i 3 -s ${BIG_SRS} -v $WORKSPACE/aggregation_vk.bin
+${ZKIT} export_aggregation_verification_key --c $i --i 3 -s ${BIG_SRS} --v $WORKSPACE/aggregation_vk.bin
 
 echo "6. generate aggregation proof"
-${ZKIT} aggregation_prove -s ${BIG_SRS} -f $OLD_PROOF_LIST  -v $WORKSPACE/vk.bin -n $WORKSPACE/aggregation_proof.bin  -j $WORKSPACE/aggregation_proof.json
+${ZKIT} aggregation_prove -s ${BIG_SRS} --f $OLD_PROOF_LIST  --v $WORKSPACE/vk.bin --n $WORKSPACE/aggregation_proof.bin  --j $WORKSPACE/aggregation_proof.json
 
 echo "7. verify"
-${ZKIT} aggregation_verify -p $WORKSPACE/aggregation_proof.bin -v $WORKSPACE/aggregation_vk.bin
+${ZKIT} aggregation_verify --p $WORKSPACE/aggregation_proof.bin --v $WORKSPACE/aggregation_vk.bin
 
 echo "8. generate verifier"
-${ZKIT} generate_aggregation_verifier -o $WORKSPACE/vk.bin -n $WORKSPACE/aggregation_vk.bin -i 3 -s $WORKSPACE/verifier.sol
+${ZKIT} generate_aggregation_verifier -o $WORKSPACE/vk.bin --n $WORKSPACE/aggregation_vk.bin --num_inputs 3 -s $WORKSPACE/verifier.sol
