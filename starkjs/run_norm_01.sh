52a5ba470693150b771324f1de13a2c4ee9ee695
cyl19970726 <15258378443@163.com>
2023-06-22 04:37:02 +0000

    run norm shell

--- starkjs/run_norm_01.sh
+++ starkjs/run_norm_01.sh
@@ -1,4 +1,4 @@
-npm run fib
+mkdir -p circuits && node fibonacci/fibonacci.js -w circuits -i "./input2.json" 
 
 ../target/release/eigen-zkit stark_prove -s ../starky/data/starkStruct.json.gl \
     -p /tmp/fib.pil.json \
