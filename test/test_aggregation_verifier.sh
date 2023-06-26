b6ec70f44ad29d3eab696db702e32af41f2db633
eigmax <stephen@ieigen.com>
2023-06-27 02:03:38 +0800

    chore: polish scripts

--- test/test_aggregation_verifier.sh
+++ test/test_aggregation_verifier.sh
@@ -7,6 +7,7 @@ cargo build --release
 BIG_POWER=26
 POWER=22
 NUM_PROOF=2
+NUM_INPUT=2
 CUR_DIR=$(cd $(dirname $0);pwd)
 ZKIT="${CUR_DIR}/../target/release/eigen-zkit"
 CIRCUIT="fibonacci"
@@ -49,7 +50,7 @@ ${ZKIT} export_verification_key -s ${SRS} -c $WORKSPACE/${RECURSIVE_CIRCUIT}.r1c
 echo "3. generate each proof"
 for (( i=0; i<$NUM_PROOF; i++ ))
 do
-    input=$CUR_DIR/aggregation/${RECURSIVE_CIRCUIT}/$i && mkdir -p $input
+    input=$CUR_DIR/aggregation/$i/${RECURSIVE_CIRCUIT} && mkdir -p $input
     ${ZKIT} calculate_witness -w ${WORKSPACE}/${RECURSIVE_CIRCUIT}_js/$RECURSIVE_CIRCUIT.wasm -i ${input}/input.json -o $input/witness.wtns
     ${ZKIT} prove -c $WORKSPACE/${RECURSIVE_CIRCUIT}.r1cs -w $input/witness.wtns --b $input/proof.bin -s ${SRS} -t rescue
 done
@@ -60,14 +61,14 @@ OLD_PROOF_LIST=$WORKSPACE/old_proof_list.txt
 
 for (( i=0; i<$NUM_PROOF; i++ ))
 do
-    input=${CUR_DIR}/aggregation/${RECURSIVE_CIRCUIT}/$i
+    input=${CUR_DIR}/aggregation/$i/${RECURSIVE_CIRCUIT}
     echo $input/proof.bin >> $OLD_PROOF_LIST
 done
 
 cat $OLD_PROOF_LIST
 
 echo "5. export aggregation vk"
-${ZKIT} export_aggregation_verification_key --c $NUM_PROOF --i 2 -s ${BIG_SRS} --v $WORKSPACE/aggregation_vk.bin
+${ZKIT} export_aggregation_verification_key --c $NUM_PROOF --i $NUM_INPUT -s ${BIG_SRS} --v $WORKSPACE/aggregation_vk.bin
 
 echo "6. generate aggregation proof"
 ${ZKIT} aggregation_prove -s ${BIG_SRS} --f $OLD_PROOF_LIST  --v $WORKSPACE/vk.bin --n $WORKSPACE/aggregation_proof.bin  --j $WORKSPACE/aggregation_proof.json
@@ -76,4 +77,4 @@ echo "7. verify"
 ${ZKIT} aggregation_verify --p $WORKSPACE/aggregation_proof.bin --v $WORKSPACE/aggregation_vk.bin
 
 echo "8. generate verifier"
-${ZKIT} generate_aggregation_verifier -o $WORKSPACE/vk.bin --n $WORKSPACE/aggregation_vk.bin --num_inputs 2 -s $WORKSPACE/verifier.sol
+${ZKIT} generate_aggregation_verifier -o $WORKSPACE/vk.bin --n $WORKSPACE/aggregation_vk.bin --num_inputs $NUM_INPUT -s $WORKSPACE/verifier.sol
