d5faae2339a6fcd0920c341a5a663b2eb4a4ef79
cyl19970726 <15258378443@163.com>
2023-07-02 15:25:32 +0000

    update stark_aggregation.sh

--- test/stark_aggregation.sh
+++ test/stark_aggregation.sh
@@ -22,11 +22,11 @@ WORKSPACE=/tmp/aggregation_$CIRCUIT
 rm -rf $WORKSPACE && mkdir -p $WORKSPACE
 
 cd ${CUR_DIR} && npm i
-# for (( i=0; i<$NUM_PROOF; i++ ))
-# do
-#     ./recursive_proof_to_stark.sh $i $WORKSPACE $CIRCUIT $PILEXECJS 
-# done
-# wait
+for (( i=0; i<$NUM_PROOF; i++ ))
+do
+    ./recursive_proof_to_stark.sh $i $WORKSPACE $CIRCUIT $PILEXECJS 
+done
+
 
 RECURSIVE_CIRCUIT=$CIRCUIT.recursive1
 RECURSIVE2_CIRCUIT=$CIRCUIT.recursive2
@@ -48,7 +48,7 @@ node $RUNDIR/src/compressor12/main_compressor12_setup.js \
     -r $WORKSPACE/$RECURSIVE_CIRCUIT.r1cs \
     -c $WORKSPACE/$RECURSIVE_CIRCUIT.const \
     -p $WORKSPACE/$RECURSIVE_CIRCUIT.pil \
-    -e $WORKSPACE/$RECURSIVE_CIRCUIT.exec
+    -e $WORKSPACE/$RECURSIVE_CIRCUIT.exec 
 
 echo "4. generate the commit polynomicals files  "
 # generate the commit polynomicals files 
@@ -61,9 +61,6 @@ node $RUNDIR/src/compressor12/main_compressor12_exec.js \
     -e $WORKSPACE/$RECURSIVE_CIRCUIT.exec \
     -m $WORKSPACE/$RECURSIVE_CIRCUIT.cm
 
-
-
-
 mkdir -p ./aggregation/$RECURSIVE2_CIRCUIT/
 
 # generate the stark proof and the circom circuits to verify stark proof.
@@ -74,5 +71,15 @@ mkdir -p ./aggregation/$RECURSIVE2_CIRCUIT/
     --o $WORKSPACE/$RECURSIVE_CIRCUIT.const \
     --m $WORKSPACE/$RECURSIVE_CIRCUIT.cm -c $RUNDIR/circuits/$RECURSIVE2_CIRCUIT.circom --i ./aggregation/$RECURSIVE2_CIRCUIT/r2_input.zkin.json 
 
-../target/release/eigen-zkit compile -p bn128 -i $RUNDIR/circuits/$RECURSIVE2_CIRCUIT.circom -l $RUNDIR/node_modules/pil-stark/circuits.bn128 --O2=full -o ./aggregation/$RECURSIVE2_CIRCUIT
+../target/release/eigen-zkit compile -p bn128 -i $RUNDIR/circuits/$RECURSIVE2_CIRCUIT.circom -l $RUNDIR/node_modules/pil-stark/circuits.bn128 --O2=full -o ./aggregation/$RECURSIVE2_CIRCUIT/
+
+cp ./aggregation/$RECURSIVE2_CIRCUIT/$RECURSIVE2_CIRCUIT.wasm /tmp/aggregation/circuits.wasm
+
+SRS=${CUR_DIR}/../keys/setup_2^${POWER}.ptau
+if [ ! -f $SRS ]; then
+    echo "download powersOfTau28_hez_final_${POWER}.ptau"
+    curl https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_${POWER}.ptau -o $SRS
+fi
+
+snarkjs g16s ./aggregation/$RECURSIVE2_CIRCUIT/$RECURSIVE2_CIRCUIT.r1cs $SRS  /tmp/aggregation/g16.zkey
 
