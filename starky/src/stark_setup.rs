cf36ba58228c65d0071aa395d4ce41f5ec9c2678
ibmp33 <2285673866@qq.com>
2023-08-17 00:33:21 +0800

    chore: customize the global_l1

--- starky/src/stark_setup.rs
+++ starky/src/stark_setup.rs
@@ -22,6 +22,7 @@ pub struct StarkSetup<M: MerkleTree> {
 ///
 ///  calculate the trace polynomial over extended field, return the new polynomial's coefficient.
 impl<M: MerkleTree> StarkSetup<M> {
+    //  https://github.com/0xEigenLabs/eigen-zkvm/pull/91
     pub fn new(
         const_pol: &PolsArray,
         pil: &mut PIL,
