d095347155f254a138ac875d451e08ac5f470be5
Paul Cheng <chengyuejia@foxmail.com>
2023-09-02 18:50:41 +0800

    Remove redunction

--- zkit/tests/bottom_layer.rs
+++
@@ -1,4 +0,0 @@
-// ../target/release/eigen-zkit stark_prove -s ./build/proof/starkstruct.json \
-// -p ./circuits/zkvm.pil.json \
-// --o ./circuits/zkvm.const \
-// --m ./circuits/zkvm.cm -c circuits/zkvm.circom --i circuits/zkvm.zkin.json
