289069ef90b3f832aca89daca1b961dec6a45b6e
eigmax <stephen@ieigen.com>
2023-09-26 11:35:30 +0000

    chore: mkdir before cp

--- test/simple_bls.sh
+++ test/simple_bls.sh
@@ -35,7 +35,8 @@ cd ../test
 # FIXME 
 CUR_DIR=$(cd $(dirname $0);pwd)
 CIRCUIT_NAME=c12a.verifier
-WORK_DIR=${CUR_DIR}/aggregation
+WORK_DIR=${CUR_DIR}/aggregation2
+mkdir -p $WORK_DIR/$CIRCUIT_NAME
 cp ../starkjs/circuits/c12a.verifier.zkin.json $WORK_DIR/$CIRCUIT_NAME/final_input.zkin.json
 
 bash -x ./snark_verifier.sh groth16 true bls12381 $CIRCUIT_NAME $WORK_DIR
