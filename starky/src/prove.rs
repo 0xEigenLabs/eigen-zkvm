602f07584cdeede045b0e73bb8af4ff3c087aa56
Paul Cheng <chengyuejia@foxmail.com>
2023-10-12 00:41:14 +0800

    refact: refact stark prove

--- starky/src/prove.rs
+++ starky/src/prove.rs
@@ -8,10 +8,12 @@ use crate::{
     stark_gen::StarkProof,
     stark_setup::StarkSetup,
     stark_verify::stark_verify,
+    traits::{MerkleTree, Transcript},
     transcript::TranscriptGL,
     transcript_bls12381::TranscriptBLS128,
     transcript_bn128::TranscriptBN128,
     types::*,
+    ElementDigest,
 };
 use std::fs::File;
 use std::io::Write;
@@ -36,179 +38,109 @@ pub fn stark_prove(
 
     let stark_struct = load_json::<StarkStruct>(stark_struct.as_str()).unwrap();
     match stark_struct.verificationHashType.as_str() {
-        "BN128" => {
-            let mut setup =
-                StarkSetup::<MerkleTreeBN128>::new(&const_pol, &mut pil, &stark_struct, None)
-                    .unwrap();
-            let mut starkproof = StarkProof::<MerkleTreeBN128>::stark_gen::<TranscriptBN128>(
-                &cm_pol,
-                &const_pol,
-                &setup.const_tree,
-                &setup.starkinfo,
-                &setup.program,
-                &pil,
-                &stark_struct,
-                prover_addr,
-            )
-            .unwrap();
-
-            log::debug!("verify the proof...");
-            let result = stark_verify::<MerkleTreeBN128, TranscriptBN128>(
-                &starkproof,
-                &setup.const_root,
-                &setup.starkinfo,
-                &stark_struct,
-                &mut setup.program,
-            )
-            .unwrap();
-
-            assert_eq!(result, true);
-            log::debug!("verify the proof done");
-
-            let opt = pil2circom::StarkOption {
-                enable_input: false,
-                verkey_input: norm_stage,
-                skip_main: false,
-                agg_stage: false,
-            };
-
-            log::debug!("generate circom");
-            let str_ver = pil2circom::pil2circom(
-                &pil,
-                &setup.const_root,
-                &stark_struct,
-                &mut setup.starkinfo,
-                &mut setup.program,
-                &opt,
-            )?;
-            let mut file = File::create(&circom_file)?;
-            write!(file, "{}", str_ver)?;
-            log::debug!("generate circom done");
-
-            if !norm_stage {
-                starkproof.rootC = None;
-            }
-
-            let input = serde_json::to_string(&starkproof)?;
-            let mut file = File::create(&zkin)?;
-            write!(file, "{}", input)?;
-            log::debug!("generate zkin done");
-        }
-        "BLS12381" => {
-            let mut setup =
-                StarkSetup::<MerkleTreeBLS12381>::new(&const_pol, &mut pil, &stark_struct, None)
-                    .unwrap();
-            let mut starkproof = StarkProof::<MerkleTreeBLS12381>::stark_gen::<TranscriptBLS128>(
-                &cm_pol,
-                &const_pol,
-                &setup.const_tree,
-                &setup.starkinfo,
-                &setup.program,
-                &pil,
-                &stark_struct,
-                prover_addr,
-            )
-            .unwrap();
-            log::debug!("verify the proof...");
-            let result = stark_verify::<MerkleTreeBLS12381, TranscriptBLS128>(
-                &starkproof,
-                &setup.const_root,
-                &setup.starkinfo,
-                &stark_struct,
-                &mut setup.program,
-            )
-            .unwrap();
-
-            assert_eq!(result, true);
-            log::debug!("verify the proof done");
-
-            let opt = pil2circom::StarkOption {
-                enable_input: false,
-                verkey_input: norm_stage,
-                skip_main: false,
-                agg_stage: false,
-            };
-
-            log::debug!("generate circom");
-            let str_ver = pil2circom::pil2circom(
-                &pil,
-                &setup.const_root,
-                &stark_struct,
-                &mut setup.starkinfo,
-                &mut setup.program,
-                &opt,
-            )?;
-            let mut file = File::create(&circom_file)?;
-            write!(file, "{}", str_ver)?;
-            log::debug!("generate circom done");
-
-            if !norm_stage {
-                starkproof.rootC = None;
-            }
-
-            let input = serde_json::to_string(&starkproof)?;
-            let mut file = File::create(&zkin)?;
-            write!(file, "{}", input)?;
-            log::debug!("generate zkin done");
-        }
-        "GL" => {
-            let mut setup =
-                StarkSetup::<MerkleTreeGL>::new(&const_pol, &mut pil, &stark_struct, None).unwrap();
-            let mut starkproof = StarkProof::<MerkleTreeGL>::stark_gen::<TranscriptGL>(
-                &cm_pol,
-                &const_pol,
-                &setup.const_tree,
-                &setup.starkinfo,
-                &setup.program,
-                &pil,
-                &stark_struct,
-                prover_addr,
-            )
-            .unwrap();
-            log::debug!("verify the proof...");
-            let result = stark_verify::<MerkleTreeGL, TranscriptGL>(
-                &starkproof,
-                &setup.const_root,
-                &setup.starkinfo,
-                &stark_struct,
-                &mut setup.program,
-            )
-            .unwrap();
-
-            assert_eq!(result, true);
-            log::debug!("verify the proof done");
-
-            let opt = pil2circom::StarkOption {
-                enable_input: false,
-                verkey_input: norm_stage,
-                skip_main: false,
-                agg_stage: agg_stage,
-            };
-
-            log::debug!("generate circom");
-            let str_ver = pil2circom::pil2circom(
-                &pil,
-                &setup.const_root,
-                &stark_struct,
-                &mut setup.starkinfo,
-                &mut setup.program,
-                &opt,
-            )?;
-            let mut file = File::create(&circom_file)?;
-            write!(file, "{}", str_ver)?;
-            log::debug!("generate circom done");
-
-            if !norm_stage {
-                starkproof.rootC = None;
-            }
-
-            let input = serde_json::to_string(&starkproof)?;
-
-            let mut file = File::create(&zkin)?;
-            write!(file, "{}", input)?;
-            log::debug!("generate zkin done");
-        }
+        "BN128" => prove::<MerkleTreeBN128, TranscriptBN128>(
+            &mut pil,
+            &const_pol,
+            &cm_pol,
+            &stark_struct,
+            false,
+            norm_stage,
+            circom_file,
+            zkin,
+            prover_addr,
+        ),
+        "BLS12381" => prove::<MerkleTreeBLS12381, TranscriptBLS128>(
+            &mut pil,
+            &const_pol,
+            &cm_pol,
+            &stark_struct,
+            false,
+            norm_stage,
+            circom_file,
+            zkin,
+            prover_addr,
+        ),
+        "GL" => prove::<MerkleTreeGL, TranscriptGL>(
+            &mut pil,
+            &const_pol,
+            &cm_pol,
+            &stark_struct,
+            agg_stage,
+            norm_stage,
+            circom_file,
+            zkin,
+            prover_addr,
+        ),
         _ => panic!("Invalid hashtype {}", stark_struct.verificationHashType),
     };
+    Ok(())
+}
+
+// Adopt with different curve, eg: BN128, BLS12381, Goldilocks
+fn prove<M: MerkleTree<MTNode = ElementDigest<4>>, T: Transcript>(
+    pil: &mut PIL,
+    const_pol: &PolsArray,
+    cm_pol: &PolsArray,
+    stark_struct: &StarkStruct,
+    agg_stage: bool,
+    norm_stage: bool,
+    circom_file: &String,
+    zkin: &String,
+    prover_addr: &String,
+) -> Result<()> {
+    let mut setup = StarkSetup::<M>::new(const_pol, pil, stark_struct, None).unwrap();
+    let mut starkproof = StarkProof::<M>::stark_gen::<T>(
+        cm_pol,
+        const_pol,
+        &setup.const_tree,
+        &setup.starkinfo,
+        &setup.program,
+        pil,
+        &stark_struct,
+        prover_addr,
+    )
+    .unwrap();
+    log::debug!("verify the proof...");
+
+    let result = stark_verify::<M, T>(
+        &starkproof,
+        &setup.const_root,
+        &setup.starkinfo,
+        stark_struct,
+        &mut setup.program,
+    )
+    .unwrap();
+
+    assert_eq!(result, true);
+    log::debug!("verify the proof done");
+
+    let opt = pil2circom::StarkOption {
+        enable_input: false,
+        verkey_input: norm_stage,
+        skip_main: false,
+        agg_stage: agg_stage,
+    };
+
+    log::debug!("generate circom");
+    let str_ver = pil2circom::pil2circom(
+        &pil,
+        &setup.const_root,
+        &stark_struct,
+        &mut setup.starkinfo,
+        &mut setup.program,
+        &opt,
+    )?;
+    let mut file = File::create(&circom_file)?;
+    write!(file, "{}", str_ver)?;
+    log::debug!("generate circom done");
+
+    if !norm_stage {
+        starkproof.rootC = None;
+    }
+
+    let input = serde_json::to_string(&starkproof)?;
+    let mut file = File::create(&zkin)?;
+    write!(file, "{}", input)?;
+    log::debug!("generate zkin done");
     Ok(())
 }
