c64dd8c820816cde7a249a3405be502edec48a12
eigmax <stephen@ieigen.com>
2023-10-12 12:19:53 +0800

    fix: script configure

--- test/snark_verifier.sh
+++ test/snark_verifier.sh
@@ -21,7 +21,7 @@ WORK_DIR=$5/$CIRCUIT_NAME
 mkdir -p $WORK_DIR
 
 SNARK_CIRCOM=$5/$CIRCUIT_NAME.circom
-SNARK_INPUT=$5/final_input.zkin.json 
+SNARK_INPUT=$5/final_input.zkin.json
 
 RUNDIR="${CUR_DIR}/../starkjs"
 
