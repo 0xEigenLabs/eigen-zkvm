e6e2c1c30cc85247a74a738b8a54d9d1f234ec56
eigmax <stephen@ieigen.com>
2023-10-19 11:05:29 +0800

    refactor: remove redundant test vectors

--- groth16/src/groth16.rs
+++ groth16/src/groth16.rs
@@ -61,11 +61,11 @@ mod tests {
     use algebraic::circom_circuit::CircomCircuit;
     use algebraic::reader;
     use algebraic::witness::{load_input_for_witness, WitnessCalculator};
-    const INPUT_FILE: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/../test/input.json");
+    const INPUT_FILE: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/../test/multiplier.input.json");
     const CIRCUIT_FILE: &'static str =
         concat!(env!("CARGO_MANIFEST_DIR"), "/../test/multiplier.r1cs");
     const WASM_FILE: &'static str =
-        concat!(env!("CARGO_MANIFEST_DIR"), "/test-vectors/mycircuit.wasm");
+        concat!(env!("CARGO_MANIFEST_DIR"), "/../test/multiplier.wasm");
     const CIRCUIT_FILE_BLS12: &'static str = concat!(
         env!("CARGO_MANIFEST_DIR"),
         "/test-vectors/mycircuit_bls12381.r1cs"
