3fc5d7f060dc50f4e2f56a6190e633c501366144
Paul Cheng <chengyuejia@foxmail.com>
2023-10-22 13:06:04 +0800

    bugifx

--- starky/benches/poseidon.rs
+++ starky/benches/poseidon.rs
@@ -2,19 +2,20 @@
 extern crate criterion;
 
 use criterion::{BenchmarkId, Criterion};
-use ff::PrimeField;
+use ff::{Field, PrimeField};
 use starky::dev::gen_rand_fields;
+use starky::field_bn128::Fr;
 use starky::poseidon_bn128_opt::Poseidon;
 const MIN_K: usize = 6;
 const MAX_K: usize = 24;
 
-fn bench_poseidon128<F: PrimeField>(c: &mut Criterion) {
+fn bench_poseidon128(c: &mut Criterion) {
     let poseidon = Poseidon::new();
-    let init = F::zero();
+    let init = Fr::zero();
 
     let mut group = c.benchmark_group("poseidon128_opt");
     for k in MIN_K..=MAX_K {
-        let a: Vec<F> = gen_rand_fields(k);
+        let a: Vec<Fr> = gen_rand_fields(k);
         group.bench_function(BenchmarkId::new("k", k), |b| {
             b.iter(|| poseidon.hash(&a, &init));
         });
@@ -22,7 +23,7 @@ fn bench_poseidon128<F: PrimeField>(c: &mut Criterion) {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    bench_poseidon128::<starky::field_bn128::Fr>(c);
+    bench_poseidon128(c);
 }
 
 criterion_group!(benches, criterion_benchmark);
