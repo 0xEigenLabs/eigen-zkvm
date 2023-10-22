6196b88fe8f029663baec8135103f6700d5bac57
Paul Cheng <chengyuejia@foxmail.com>
2023-10-22 12:16:18 +0800

    feat: add fft bench

--- starky/benches/fft.rs
+++ starky/benches/fft.rs
@@ -2,32 +2,47 @@
 extern crate criterion;
 
 use criterion::{BenchmarkId, Criterion};
+use rayon::prelude::*;
 use starky::fft_p::{fft, ifft};
 use starky::traits::FieldExtension;
 use starky::{f3g::F3G, fft::FFT};
-
+use std::fmt::format;
 const MIN_K: usize = 6;
 const MAX_K: usize = 24;
+
+fn generate_coeffs<F: FieldExtension>(k: usize) -> Vec<F> {
+    let mut rng = ::rand::thread_rng();
+
+    let num_threads = rayon::current_num_threads();
+    let mut parts = vec![F::one(); 1 << k];
+    rayon::scope(|scope| {
+        for out in parts.chunks_mut(num_threads) {
+            scope.spawn(move |_| {
+                for i in 0..num_threads {
+                    out[i] = <F as rand::Rand>::rand(&mut rng)
+                }
+            })
+        }
+    });
+    parts
+}
 
 fn bench_standard_fft<F: FieldExtension>(c: &mut Criterion) {
     let mut f = FFT::new();
 
-    let mut group_fft = c.benchmark_group("fft");
-    let mut group_ifft = c.benchmark_group("ifft");
+    let mut group_fft = c.benchmark_group("fft & ifft");
     let mut rng = ::rand::thread_rng();
     for k in MIN_K..=MAX_K {
         // prepare data.
-        let mut a = (0..(1 << k))
-            .map(|_| <F3G as rand::Rand>::rand(&mut rng))
-            .collect::<Vec<_>>();
+        let mut a: Vec<F> = generate_coeffs(k);
         // bench fft
-        group_fft.bench_function(BenchmarkId::new("k", k), |b| {
+        group_fft.bench_function(format!("fft,k={}", k), |b| {
             b.iter(|| {
                 f.fft(&a);
             });
         });
         // bench ifft
-        group_ifft.bench_function(BenchmarkId::new("k", k), |b| {
+        group_fft.bench_function(format!("ifft,k={}", k), |b| {
             b.iter(|| {
                 f.ifft(&a);
             });
@@ -36,22 +51,19 @@ fn bench_standard_fft<F: FieldExtension>(c: &mut Criterion) {
 }
 
 fn bench_p_fft<F: FieldExtension>(c: &mut Criterion) {
-    let mut group_fft = c.benchmark_group("p_fft");
-    let mut group_ifft = c.benchmark_group("p_ifft");
+    let mut group_fft = c.benchmark_group("p_fft & p_ifft");
     let mut rng = ::rand::thread_rng();
     for k in MIN_K..=MAX_K {
         // prepare data.
-        let mut a = (0..(1 << k))
-            .map(|_| <F3G as rand::Rand>::rand(&mut rng))
-            .collect::<Vec<_>>();
+        let mut a: Vec<F> = generate_coeffs(k);
         // bench fft
-        group_fft.bench_function(BenchmarkId::new("k", k), |b| {
+        group_fft.bench_function(format!("p_fft,k={}", k), |b| {
             b.iter(|| {
                 fft(&a, 1, k, &mut vec![]);
             });
         });
         // bench ifft
-        group_ifft.bench_function(BenchmarkId::new("k", k), |b| {
+        group_fft.bench_function(format!("p_ifft,k={}", k), |b| {
             b.iter(|| {
                 ifft(&a, 1, k, &mut vec![]);
             });
