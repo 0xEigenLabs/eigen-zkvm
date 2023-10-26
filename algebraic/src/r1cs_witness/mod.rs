fcc5c14cd8cd9046321601aa71dbfb68bbc85e0c
Paul Cheng <chengyuejia@foxmail.com>
2023-10-26 21:52:18 +0800

    clippy

--- algebraic/src/r1cs_witness/mod.rs
+++ algebraic/src/r1cs_witness/mod.rs
@@ -13,8 +13,7 @@ use std::str::FromStr;
 
 pub fn load_input_for_witness(input_file: &str) -> HashMap<String, Vec<BigInt>> {
     let inputs_str = std::fs::read_to_string(input_file).unwrap();
-    let inputs: std::collections::HashMap<String, serde_json::Value> =
-        serde_json::from_str(&inputs_str).unwrap();
+    let inputs: HashMap<String, Value> = serde_json::from_str(&inputs_str).unwrap();
 
     inputs
         .iter()
@@ -32,14 +31,12 @@ pub fn load_input_for_witness(input_file: &str) -> HashMap<String, Vec<BigInt>> 
                 }
                 Value::Number(inner) => {
                     vec![BigInt::from_str(&inner.to_string()).unwrap()]
-                    //vec![BigInt::from(inner.as_u64().expect("not a u32"))]
                 }
-                //Value::Array(inner) => inner.iter().cloned().map(value_to_bigint).collect(),
                 Value::Array(inner) => flat_array(inner),
                 _ => panic!("{:?}", value),
             };
 
             (key.clone(), res)
         })
-        .collect::<std::collections::HashMap<_, _>>()
+        .collect::<HashMap<_, _>>()
 }
