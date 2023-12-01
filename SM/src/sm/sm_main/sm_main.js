b0d9786c063844ee18548202a197e5476ec5bcb9
Jango <96159517+JangoCCC@users.noreply.github.com>
2023-12-01 01:22:21 +0000

    refactor: migrate executor

--- SM/src/sm/sm_main/sm_main.js
+++
@@ -1,2 +0,0 @@
-module.exports.execute = require("./sm_main_exec");
-module.exports.buildConstants = require("./sm_main_const");
--- SM/tools/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-BDIR=${npm_config_build:=build/proof}
-NODE_OPTIONS="--max-old-space-size=819200"
-true
-
