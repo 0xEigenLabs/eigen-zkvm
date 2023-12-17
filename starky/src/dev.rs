0c0978a28e26c38bb1e118064e450f7c2ba73bce
Paul Cheng <chengyuejia@foxmail.com>
2023-12-17 10:49:11 +0800

    feat: refact

--- starky/src/dev.rs
+++ starky/src/dev.rs
@@ -1,59 +1,31 @@
 /// A test/bench tools
 use crate::traits::FieldExtension;
 use ff::PrimeField;
+use rayon::prelude::*;
 
 // concurrency generate random goldfields. with specific k.
 pub fn gen_rand_goldfields<F: FieldExtension>(k: usize) -> Vec<F> {
-    let num_threads = rayon::current_num_threads();
     let n = 1 << k;
     let mut parts = vec![F::zero(); n];
 
-    if n < num_threads {
+    parts.par_iter_mut().for_each(|p| {
         let mut rng = ::rand::thread_rng();
-        for i in 0..n {
-            parts[i] = <F as rand::Rand>::rand(&mut rng)
-        }
-        parts
-    } else {
-        rayon::scope(|scope| {
-            for out in parts.chunks_mut(num_threads) {
-                scope.spawn(move |_| {
-                    let mut rng = ::rand::thread_rng();
-                    for i in 0..num_threads {
-                        out[i] = <F as rand::Rand>::rand(&mut rng)
-                    }
-                })
-            }
-        });
-        parts
-    }
+        *p = <F as rand::Rand>::rand(&mut rng)
+    });
+    parts
 }
 
 // concurrency generate random fields. with specific k.
 pub fn gen_rand_fields<F: PrimeField>(k: usize) -> Vec<F> {
     let n = 1 << k;
-    let num_threads = rayon::current_num_threads();
     let mut parts = vec![F::zero(); n];
 
-    if n < num_threads {
+    parts.par_iter_mut().for_each(|p| {
         let mut rng = ::rand::thread_rng();
-        for i in 0..n {
-            parts[i] = <F as rand::Rand>::rand(&mut rng)
-        }
-        parts
-    } else {
-        rayon::scope(|scope| {
-            for out in parts.chunks_mut(num_threads) {
-                scope.spawn(move |_| {
-                    let mut rng = ::rand::thread_rng();
-                    for i in 0..num_threads {
-                        out[i] = <F as rand::Rand>::rand(&mut rng)
-                    }
-                })
-            }
-        });
-        parts
-    }
+
+        *p = <F as rand::Rand>::rand(&mut rng)
+    });
+    parts
 }
 
 #[cfg(test)]
