048efc2552ab6017ad817a890d3d15f392154673
Stephen <81497928+eigmax@users.noreply.github.com>
2024-01-21 13:13:33 +0800

    fix: zkvm revm broken (#193)
    * fix: REVM update
    
    * chore: use powdr main branch
    
    * chore: use powdr main branch
    
    * chore: use powdr main branch

--- zkvm/src/lib.rs
+++ zkvm/src/lib.rs
@@ -1,22 +1,15 @@
 use backend::BackendType;
-use powdr::executor::witgen::QueryCallback;
-use powdr::number::{FieldElement, GoldilocksField};
-use powdr::pipeline::{parse_query, Pipeline, Stage};
+use powdr::number::GoldilocksField;
+use powdr::pipeline::{Pipeline, Stage};
 use powdr::riscv::continuations::{
     bootloader::default_input, rust_continuations, rust_continuations_dry_run,
 };
 use powdr::riscv::{compile_rust, CoProcessors};
 use powdr::riscv_executor;
-use std::collections::HashMap as STDHashMap;
 use std::path::Path;
 use std::time::Instant;
 
-pub fn zkvm_evm_prove_one(
-    suite_json: String,
-    addr: &str,
-    chain_id: u32,
-    output_path: &str,
-) -> Result<(), String> {
+pub fn zkvm_evm_prove_one(suite_json: String, output_path: &str) -> Result<(), String> {
     log::debug!("Compiling Rust...");
     let force_overwrite = true;
     let with_bootloader = true;
@@ -33,6 +26,7 @@ pub fn zkvm_evm_prove_one(
     let mk_pipeline = || {
         Pipeline::<GoldilocksField>::default()
             .from_asm_string(asm_contents.clone(), Some(asm_file_path.clone()))
+            .with_prover_inputs(vec![])
     };
 
     log::debug!("Creating pipeline from powdr-asm...");
@@ -47,33 +41,19 @@ pub fn zkvm_evm_prove_one(
     let duration = start.elapsed();
     log::debug!("Advancing pipeline took: {:?}", duration);
 
-    let mk_pipeline_opt =
-        || mk_pipeline().from_pil_with_evaluated_fixed_cols(pil_with_evaluated_fixed_cols.clone());
+    let mk_pipeline_with_data = || mk_pipeline().add_data(666, &suite_json);
 
-    log::debug!("Creating data callback...");
-    let mut suite_json_bytes: Vec<GoldilocksField> = suite_json
-        .into_bytes()
-        .iter()
-        .map(|b| (*b as u32).into())
-        .collect();
-    suite_json_bytes.insert(0, (suite_json_bytes.len() as u32).into());
-
-    let mut data: STDHashMap<GoldilocksField, Vec<GoldilocksField>> = STDHashMap::default();
-    data.insert(666.into(), suite_json_bytes);
-    data.insert(
-        667.into(),
-        [1, chain_id].iter().map(|b| (*b).into()).collect(),
-    );
-    let mut addr: Vec<_> = addr.as_bytes().iter().map(|b| (*b as u32).into()).collect();
-    addr.insert(0, (addr.len() as u32).into());
-    data.insert(668.into(), addr);
+    let mk_pipeline_opt = || {
+        mk_pipeline_with_data()
+            .from_pil_with_evaluated_fixed_cols(pil_with_evaluated_fixed_cols.clone())
+    };
 
     log::debug!("Running powdr-riscv executor in fast mode...");
     let start = Instant::now();
     let (trace, _mem) = riscv_executor::execute::<GoldilocksField>(
         &asm_contents,
-        &data,
-        &default_input(),
+        mk_pipeline_with_data().data_callback().unwrap(),
+        &default_input(&[]),
         riscv_executor::ExecMode::Fast,
     );
     let duration = start.elapsed();
@@ -82,77 +62,39 @@ pub fn zkvm_evm_prove_one(
 
     log::debug!("Running powdr-riscv executor in trace mode for continuations...");
     let start = Instant::now();
-    let bootloader_inputs = rust_continuations_dry_run(mk_pipeline(), data.clone());
+    let bootloader_inputs = rust_continuations_dry_run(mk_pipeline_with_data());
     let duration = start.elapsed();
     log::debug!("Trace executor took: {:?}", duration);
 
     let prove_with = Some(BackendType::EStark);
-    let generate_witness = |pipeline: Pipeline<GoldilocksField>| -> Result<(), Vec<String>> {
-        let data = data_to_query_callback(data.clone());
-        let mut pipeline = pipeline.add_query_callback(Box::new(data));
-        pipeline.advance_to(Stage::GeneratedWitness)?;
-        prove_with.map(|backend| pipeline.with_backend(backend).proof().unwrap());
-        Ok(())
-    };
+
+    let generate_witness_and_prove =
+        |mut pipeline: Pipeline<GoldilocksField>| -> Result<(), Vec<String>> {
+            let start = Instant::now();
+            log::debug!("Generating witness...");
+            pipeline.advance_to(Stage::GeneratedWitness)?;
+            let duration = start.elapsed();
+            log::debug!("Generating witness took: {:?}", duration);
+
+            let start = Instant::now();
+            log::debug!("Proving ...");
+            prove_with.map(|backend| pipeline.with_backend(backend).proof().unwrap());
+            let duration = start.elapsed();
+            log::debug!("Proving took: {:?}", duration);
+            Ok(())
+        };
 
     log::debug!("Running witness generation...");
     let start = Instant::now();
-    rust_continuations(mk_pipeline_opt, generate_witness, bootloader_inputs).unwrap();
+    rust_continuations(
+        mk_pipeline_opt,
+        generate_witness_and_prove,
+        bootloader_inputs,
+    )
+    .unwrap();
     let duration = start.elapsed();
     log::debug!("Witness generation took: {:?}", duration);
     Ok(())
-}
-
-fn access_element<T: FieldElement>(
-    name: &str,
-    elements: &[T],
-    index_str: &str,
-) -> Result<Option<T>, String> {
-    let index = index_str
-        .parse::<usize>()
-        .map_err(|e| format!("Error parsing index: {e})"))?;
-    let value = elements.get(index).cloned();
-    if let Some(value) = value {
-        Ok(Some(value))
-    } else {
-        Err(format!(
-            "Error accessing {name}: Index {index} out of bounds {}",
-            elements.len()
-        ))
-    }
-}
-
-#[allow(clippy::print_stdout)]
-fn data_to_query_callback<T: FieldElement>(data: STDHashMap<T, Vec<T>>) -> impl QueryCallback<T> {
-    move |query: &str| -> Result<Option<T>, String> {
-        // TODO In the future, when match statements need to be exhaustive,
-        // This function probably gets an Option as argument and it should
-        // answer None by Ok(None).
-
-        match &parse_query(query)?[..] {
-            ["\"input\"", index] => access_element("prover inputs", &data[&T::zero()], index),
-            ["\"data\"", index, what] => {
-                let what = what
-                    .parse::<usize>()
-                    .map_err(|e| format!("Error parsing what: {e})"))?;
-
-                access_element("prover inputs", &data[&(what as u64).into()], index)
-            }
-            ["\"print_char\"", ch] => {
-                print!(
-                    "{}",
-                    ch.parse::<u8>()
-                        .map_err(|e| format!("Invalid char to print: {e}"))?
-                        as char
-                );
-                // We do not answer None because we don't want this function to be
-                // called again.
-                Ok(Some(0.into()))
-            }
-            ["\"hint\"", value] => Ok(Some(T::from_str(value))),
-            _k => Err("Unsupported query".to_string()),
-        }
-    }
 }
 
 #[cfg(test)]
@@ -199,7 +141,6 @@ mod tests {
         .into();
         */
 
-        let addr = "a94f5374fce5edbc8e2a8697c15331677e6ebf0b";
-        zkvm_evm_prove_one(suite_json, addr, 1, "/tmp/test").unwrap();
+        zkvm_evm_prove_one(suite_json, "/tmp/test").unwrap();
     }
 }
