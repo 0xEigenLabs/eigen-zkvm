b335daeaa4611b443d88a158735bd0b4a88baa8c
eigmax <stephen@ieigen.com>
2024-01-27 00:36:35 +0800

    chore: execute then prove

--- zkvm/src/lib.rs
+++ zkvm/src/lib.rs
@@ -9,7 +9,7 @@ use powdr_riscv::{compile_rust, CoProcessors};
 use std::path::Path;
 use std::time::Instant;
 
-pub fn zkvm_evm_prove_one(task: &str, suite_json: String, output_path: &str) -> Result<()> {
+pub fn zkvm_evm_execute_and_prove(task: &str, suite_json: String, output_path: &str) -> Result<()> {
     log::debug!("Compiling Rust...");
     let force_overwrite = true;
     let with_bootloader = true;
@@ -98,9 +98,110 @@ pub fn zkvm_evm_prove_one(task: &str, suite_json: String, output_path: &str) -> 
     Ok(())
 }
 
+pub fn zkvm_evm_generate_chunks(
+    task: &str,
+    suite_json: &String,
+    output_path: &str,
+) -> Result<Vec<Vec<GoldilocksField>>> {
+    log::debug!("Compiling Rust...");
+    let force_overwrite = true;
+    let with_bootloader = true;
+    let (asm_file_path, asm_contents) = compile_rust(
+        &format!("vm/{task}"),
+        Path::new(output_path),
+        force_overwrite,
+        &CoProcessors::base().with_poseidon(),
+        with_bootloader,
+    )
+    .ok_or_else(|| vec!["could not compile rust".to_string()])
+    .unwrap();
+
+    let mk_pipeline = || {
+        Pipeline::<GoldilocksField>::default()
+            .with_output(output_path.into(), true)
+            .from_asm_string(asm_contents.clone(), Some(asm_file_path.clone()))
+            .with_prover_inputs(vec![])
+    };
+
+    let mk_pipeline_with_data = || mk_pipeline().add_data(666, suite_json);
+
+    log::debug!("Running powdr-riscv executor in fast mode...");
+
+    let (trace, _mem) = powdr_riscv_executor::execute::<GoldilocksField>(
+        &asm_contents,
+        mk_pipeline_with_data().data_callback().unwrap(),
+        &default_input(&[]),
+        powdr_riscv_executor::ExecMode::Fast,
+    );
+    log::debug!("Trace length: {}", trace.len);
+
+    log::debug!("Running powdr-riscv executor in trace mode for continuations...");
+    let start = Instant::now();
+    let bootloader_inputs = rust_continuations_dry_run(mk_pipeline_with_data());
+    let duration = start.elapsed();
+    log::debug!(
+        "Trace executor took: {:?}, input size: {:?}",
+        duration,
+        bootloader_inputs[0].len()
+    );
+    Ok(bootloader_inputs)
+}
+
+pub fn zkvm_evm_prove_only(
+    task: &str,
+    suite_json: &String,
+    bootloader_inputs: Vec<Vec<GoldilocksField>>,
+    output_path: &str,
+) -> Result<()> {
+    log::debug!("Compiling Rust...");
+    let asm_file_path = Path::new(output_path).join(format!("{}.asm", task));
+
+    let mk_pipeline = || {
+        Pipeline::<GoldilocksField>::default()
+            .with_output(output_path.into(), true)
+            .from_asm_file(asm_file_path.clone())
+            .with_prover_inputs(vec![])
+    };
+    let mk_pipeline_with_data = || mk_pipeline().add_data(666, suite_json);
+
+    let prove_with = Some(BackendType::EStark);
+    let generate_witness_and_prove =
+        |mut pipeline: Pipeline<GoldilocksField>| -> Result<(), Vec<String>> {
+            let start = Instant::now();
+            log::debug!("Generating witness...");
+            pipeline.advance_to(Stage::GeneratedWitness)?;
+            let duration = start.elapsed();
+            log::debug!("Generating witness took: {:?}", duration);
+
+            let start = Instant::now();
+            log::debug!("Proving ...");
+            prove_with.map(|backend| pipeline.with_backend(backend).proof().unwrap());
+            let duration = start.elapsed();
+            log::debug!("Proving took: {:?}", duration);
+            Ok(())
+        };
+
+    log::debug!("Running witness generation...");
+    let start = Instant::now();
+    rust_continuations(
+        mk_pipeline_with_data,
+        generate_witness_and_prove,
+        bootloader_inputs,
+    )
+    .unwrap();
+    let duration = start.elapsed();
+    log::debug!("Witness generation took: {:?}", duration);
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
-    use super::zkvm_evm_prove_one;
+    use super::*;
+    use num_traits::identities::Zero;
+    use powdr_number::FieldElement;
+    use std::io::{Read, Write};
+
+    use std::fs;
 
     //use revm::primitives::address;
 
@@ -111,9 +212,9 @@ mod tests {
         env_logger::try_init().unwrap_or_default();
         //let test_file = "test-vectors/blockInfo.json";
         let test_file = "test-vectors/solidityExample.json";
-        let suite_json = std::fs::read_to_string(test_file).unwrap();
+        let suite_json = fs::read_to_string(test_file).unwrap();
 
-        zkvm_evm_prove_one("evm", suite_json, "/tmp/test_evm").unwrap();
+        zkvm_evm_execute_and_prove("evm", suite_json, "/tmp/test_evm").unwrap();
     }
 
     #[test]
@@ -122,8 +223,50 @@ mod tests {
         env_logger::try_init().unwrap_or_default();
         //let test_file = "test-vectors/blockInfo.json";
         let test_file = "test-vectors/solidityExample.json";
-        let suite_json = std::fs::read_to_string(test_file).unwrap();
+        let suite_json = fs::read_to_string(test_file).unwrap();
 
-        zkvm_evm_prove_one("lr", suite_json, "/tmp/test_lr").unwrap();
+        zkvm_evm_execute_and_prove("lr", suite_json, "/tmp/test_lr").unwrap();
+    }
+
+    #[test]
+    #[ignore]
+    fn test_zkvm_lr_execute_then_prove() {
+        env_logger::try_init().unwrap_or_default();
+        //let test_file = "test-vectors/blockInfo.json";
+        let test_file = "test-vectors/solidityExample.json";
+        let suite_json = fs::read_to_string(test_file).unwrap();
+
+        let output_path = "/tmp/test_lr";
+        let task = "lr";
+        let bootloader_inputs = zkvm_evm_generate_chunks(task, &suite_json, output_path).unwrap();
+        // save the chunks
+        let bi_files: Vec<_> = (0..bootloader_inputs.len())
+            .map(|i| Path::new(output_path).join(format!("{task}_chunks_{i}.data")))
+            .collect();
+        bootloader_inputs
+            .iter()
+            .zip(&bi_files)
+            .for_each(|(data, filename)| {
+                let mut f = fs::File::create(filename).unwrap();
+                for d in data {
+                    f.write_all(&d.to_bytes_le()[0..8]).unwrap();
+                }
+            });
+
+        // load each chunk, generate witness and prove
+        bi_files.iter().for_each(|filename| {
+            let mut f = fs::File::open(filename).unwrap();
+            let metadata = fs::metadata(filename).unwrap();
+            let file_size = metadata.len() as usize;
+            assert!(file_size % 8 == 0);
+            let mut buffer = vec![0; file_size];
+            f.read_exact(&mut buffer).unwrap();
+            let mut bi = vec![GoldilocksField::zero(); file_size / 8];
+            bi.iter_mut().zip(buffer.chunks(8)).for_each(|(out, bin)| {
+                *out = GoldilocksField::from_bytes_le(bin);
+            });
+
+            zkvm_evm_prove_only(task, &suite_json, vec![bi], output_path).unwrap();
+        });
     }
 }
