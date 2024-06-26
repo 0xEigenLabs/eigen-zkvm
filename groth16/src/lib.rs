cd8845d03f57adaa2b79a944525d404e3e6d9d41
ibmp33 <2285673866@qq.com>
2024-06-27 01:31:11 +0800

    chore: add gpu support for groth16

--- groth16/src/lib.rs
+++ groth16/src/lib.rs
@@ -5,21 +5,25 @@ pub mod groth16;
 pub mod json_utils;
 mod template;
 
-// #[cfg(not(any(feature = "cuda", feature = "opencl")))]
-// pub use bellman_ce::pairing::ff;
-// #[cfg(not(any(feature = "cuda", feature = "opencl")))]
-// pub use ff::*;
-// #[cfg(not(any(feature = "cuda", feature = "opencl")))]
-// pub use franklin_crypto::bellman as bellman_ce;
+// #[cfg(any(feature = "cuda", feature = "opencl"))]
+// mod gpu_specific {
+    pub mod witness;
+    pub mod circuit;
+    pub mod r1cs_file;
+    pub mod reader;
+    
+    pub use bellperson;
+// }
 
 // #[cfg(any(feature = "cuda", feature = "opencl"))]
-pub mod witness;
-// #[cfg(any(feature = "cuda", feature = "opencl"))]
-pub mod circuit;
-// #[cfg(any(feature = "cuda", feature = "opencl"))]
-pub mod r1cs_file;
-// #[cfg(any(feature = "cuda", feature = "opencl"))]
-pub mod reader;
+// pub use gpu_specific::*;
 
-// #[cfg(any(feature = "cuda", feature = "opencl"))]
-pub use bellperson;
+// #[cfg(not(any(feature = "cuda", feature = "opencl")))]
+// mod non_gpu_specific {
+//     pub use bellman_ce::pairing::ff;
+//     pub use ff::*;
+//     pub use franklin_crypto::bellman as bellman_ce;
+// }
+
+// #[cfg(not(any(feature = "cuda", feature = "opencl")))]
+// pub use non_gpu_specific::*;
