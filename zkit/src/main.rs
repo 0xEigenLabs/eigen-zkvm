c970bc6b5037b50b8e9145cba88ea42369a72731
eigmax <stephen@ieigen.com>
2024-07-27 02:49:03 +0000

    feat: sunset plonky

--- zkit/src/main.rs
+++ zkit/src/main.rs
@@ -2,11 +2,6 @@ extern crate clap;
 use clap::{command, Parser};
 use dsl_compile::circom_compiler;
 use groth16::api::*;
-use plonky::api::{
-    aggregation_check, aggregation_prove, aggregation_verify, analyse, calculate_witness,
-    export_aggregation_verification_key, export_verification_key, generate_aggregation_verifier,
-    prove as plonky_prove, setup, verify,
-};
 use starky::prove::stark_prove;
 use std::time::Instant;
 
@@ -338,38 +333,14 @@ pub struct Groth16VerifyOpt {
 
 #[derive(Parser, Debug)]
 enum Command {
-    #[command(name = "setup")]
-    Setup(SetupOpt),
-    #[command(name = "calculate_witness")]
-    CalculateWitness(CalculateWitnessOpt),
     /// Compile circom circuits to r1cs, and generate witness
     #[command(name = "compile")]
     Compile(CompilierOpt),
-    #[command(name = "prove")]
-    PlonkProve(PlonkProveOpt),
-    #[command(name = "verify")]
-    Verify(VerifyOpt),
-    #[command(name = "export_verification_key")]
-    ExportVerificationKey(ExportVerificationKeyOpt),
     #[command(name = "generate_verifier")]
     GenerateVerifier(GenerateVerifierOpt),
-    #[command(name = "export_aggregation_verification_key")]
-    ExportAggregationVerificationKey(ExportAggregationVerificationKeyOpt),
-    #[command(name = "aggregation_prove")]
-    AggregationProve(AggregationProveOpt),
-    #[command(name = "aggregation_verify")]
-    AggregationVerify(AggregationVerifyOpt),
-    #[command(name = "generate_aggregation_verifier")]
-    GenerateAggregationVerifier(GenerateAggregationVerifierOpt),
-    #[command(name = "aggregation_check")]
-    AggregationCheck(AggregationCheckOpt),
 
     #[command(name = "stark_prove")]
     StarkProve(StarkProveOpt),
-
-    #[command(name = "analyse")]
-    Analyse(AnalyseOpt),
-
     #[command(name = "compressor12_setup")]
     Compressor12Setup(Compressor12SetupOpt),
     #[command(name = "compressor12_exec")]
@@ -397,18 +368,6 @@ fn main() {
     env_logger::init();
     let start = Instant::now();
     let exec_result = match args.command {
-        Command::Setup(args) => {
-            let path = std::path::Path::new(&args.srs_monomial_form);
-            assert!(
-                !path.exists(),
-                "duplicate srs_monomial_form file: {}",
-                path.display()
-            );
-            let writer =
-                std::fs::File::create(&args.srs_monomial_form).expect("can not create file");
-            log::debug!("srs_monomial_form saved to {}", &args.srs_monomial_form);
-            setup(args.power, writer)
-        }
         Command::Compile(args) => circom_compiler(
             args.input,
             args.prime.to_lowercase(),
@@ -418,51 +377,13 @@ fn main() {
             args.no_simplification,
             args.reduced_simplification,
         ),
-        Command::CalculateWitness(args) => {
-            calculate_witness(&args.wasm_file, &args.input_json, &args.output)
-        }
-        Command::PlonkProve(args) => plonky_prove(
-            &args.circuit_file,
-            &args.witness,
-            &args.srs_monomial_form,
-            args.srs_lagrange_form,
-            &args.transcript,
-            &args.proof_bin,
-            &args.proof_json,
-            &args.public_json,
-        ),
-        Command::Verify(args) => verify(&args.vk_file, &args.proof_bin, &args.transcript),
         Command::GenerateVerifier(args) => match args.protocal.as_str() {
-            "plonk" => plonky::api::generate_verifier(&args.vk_file, &args.sol),
+            //"plonk" => plonky::api::generate_verifier(&args.vk_file, &args.sol),
             "groth16" => groth16::api::generate_verifier(&args.vk_file, &args.sol),
             _ => {
                 panic!("unknown protocol")
             }
         },
-        Command::ExportVerificationKey(args) => {
-            export_verification_key(&args.srs_monomial_form, &args.circuit_file, &args.output_vk)
-        }
-
-        Command::ExportAggregationVerificationKey(args) => export_aggregation_verification_key(
-            args.num_proofs_to_check,
-            args.num_inputs,
-            &args.srs_monomial_form,
-            &args.output_vk,
-        ),
-        Command::AggregationProve(args) => aggregation_prove(
-            &args.srs_monomial_form,
-            &args.old_proof_list,
-            &args.old_vk,
-            &args.new_proof,
-            &args.proof_json,
-        ),
-        Command::AggregationVerify(args) => aggregation_verify(&args.proof, &args.vk),
-        Command::GenerateAggregationVerifier(args) => {
-            generate_aggregation_verifier(&args.old_vk, &args.new_vk, args.num_inputs, &args.sol)
-        }
-        Command::AggregationCheck(args) => {
-            aggregation_check(&args.old_proof_list, &args.old_vk, &args.new_proof)
-        }
         Command::StarkProve(args) => stark_prove(
             &args.stark_struct,
             &args.piljson,
@@ -475,7 +396,6 @@ fn main() {
             &args.zkin,
             &args.prover_addr,
         ),
-        Command::Analyse(args) => analyse(&args.circuit_file, &args.output),
         Command::Compressor12Setup(args) => recursion::compressor12_setup::setup(
             &args.r1cs_file,
             &args.pil_file,
